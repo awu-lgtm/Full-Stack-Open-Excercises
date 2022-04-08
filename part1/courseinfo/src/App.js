b80be525c8ab00706bdfced8275cb10c6f4bb14d
awu <allen.wu72@gmail.com>
2022-04-08 00:15:58 -0400

    finish course info

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -1,7 +1,12 @@
 import React from 'react'
 
 const Header = (props) => (<div><h1>{props.course}</h1></div>)
-const Content = (props) => (<div><p>{props.part} {props.exercises}</p></div>)
+const Part = (props) => (<div><p>{props.part} {props.exercises}</p></div>)
+const Content = (props) => (<div>
+  <Part part = {props.part1} exercises = {props.exercises1}/>
+  <Part part = {props.part2} exercises = {props.exercises2}/>
+  <Part part = {props.part3} exercises = {props.exercises3}/>
+  </div>)
 const Total = (props) => (<div><p>Number of exercises {props.exercises1 + props.exercises2 + props.exercises3} </p></div>)
 
 const App = () => {
@@ -16,9 +21,7 @@ const App = () => {
   return (
     <div>
       <Header course = {course}/>
-      <Content part = {part1} exercises = {exercises1}/>
-      <Content part = {part2} exercises = {exercises2}/>
-      <Content part = {part3} exercises = {exercises3}/>
+      <Content part1 = {part1} part2 = {part2} part3 = {part3} exercises1 = {exercises1} exercises2 = {exercises2} exercises3 = {exercises3}/>
       <Total exercises1 = {exercises1} exercises2 = {exercises2} exercises3= {exercises3}/>
     </div>
   )
