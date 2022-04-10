5c034279f81c2ae6ff7acc7b88b3ee03dccfad01
awu <allen.wu72@gmail.com>
2022-04-10 00:39:15 -0400

    anecdotes

--- part1/anecdotes/src/App.js
+++ part1/anecdotes/src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
 
-function App() {
+const App = () => {
+  const anecdotes = [
+    'If it hurts, do it more often',
+    'Adding manpower to a late software project makes it later!',
+    'The first 90 percent of the code accounts for the first 10 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+    'Premature optimization is the root of all evil.',
+    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
+    'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients'
+  ]
+   
+  const [selected, setSelected] = useState(0)
+  const [points, setPoints] = useState(Array(anecdotes.length).fill(0, 0))
+
+  const increasePoints = () => {
+    let copy = [...points]
+    copy[selected] += 1
+    setPoints(copy)
+  }
+  console.log(selected)
+  console.log(points)
+
+  console.log(points.indexOf(Math.max(...points)))
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1> Anecdote of the Day </h1>
+      <p> 
+        {anecdotes[selected]}
+        <br/>
+        has {points[selected]} votes
+      </p>
+
+      <button onClick={increasePoints}> vote </button>
+      <button onClick={() => setSelected(Math.floor(Math.random() * (anecdotes.length)))}> next anecdote </button>
+    
+      <h1> Anecdote with most votes </h1>
+      <p>{anecdotes[points.indexOf(Math.max(...points))]}</p>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
